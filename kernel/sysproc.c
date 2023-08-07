30edd61e8c74a4a2def8815bfd3ecacc18c7556e
luzhixing12345 <luzhixing12345@163.com>
2023-08-07 21:48:38 +0800

    [feat] : finish lab3

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,97 +6,96 @@
 #include "spinlock.h"
 #include "proc.h"
 
-uint64
-sys_exit(void)
-{
-  int n;
-  argint(0, &n);
-  exit(n);
-  return 0;  // not reached
+uint64 sys_exit(void) {
+    int n;
+    argint(0, &n);
+    exit(n);
+    return 0;  // not reached
 }
 
-uint64
-sys_getpid(void)
-{
-  return myproc()->pid;
+uint64 sys_getpid(void) {
+    return myproc()->pid;
 }
 
-uint64
-sys_fork(void)
-{
-  return fork();
+uint64 sys_fork(void) {
+    return fork();
 }
 
-uint64
-sys_wait(void)
-{
-  uint64 p;
-  argaddr(0, &p);
-  return wait(p);
+uint64 sys_wait(void) {
+    uint64 p;
+    argaddr(0, &p);
+    return wait(p);
 }
 
-uint64
-sys_sbrk(void)
-{
-  uint64 addr;
-  int n;
+uint64 sys_sbrk(void) {
+    uint64 addr;
+    int n;
 
-  argint(0, &n);
-  addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
-  return addr;
+    argint(0, &n);
+    addr = myproc()->sz;
+    if (growproc(n) < 0)
+        return -1;
+    return addr;
 }
 
-uint64
-sys_sleep(void)
-{
-  int n;
-  uint ticks0;
+uint64 sys_sleep(void) {
+    int n;
+    uint ticks0;
 
-
-  argint(0, &n);
-  acquire(&tickslock);
-  ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(killed(myproc())){
-      release(&tickslock);
-      return -1;
+    argint(0, &n);
+    acquire(&tickslock);
+    ticks0 = ticks;
+    while (ticks - ticks0 < n) {
+        if (killed(myproc())) {
+            release(&tickslock);
+            return -1;
+        }
+        sleep(&ticks, &tickslock);
     }
-    sleep(&ticks, &tickslock);
-  }
-  release(&tickslock);
-  return 0;
+    release(&tickslock);
+    return 0;
 }
-
 
 #ifdef LAB_PGTBL
-int
-sys_pgaccess(void)
-{
-  // lab pgtbl: your code here.
-  return 0;
+int sys_pgaccess(void) {
+    
+    uint64 addr, mask_addr;
+    int len;
+    argaddr(0, &addr);
+    argint(1, &len);
+    argaddr(2, &mask_addr);
+
+    struct proc *p = myproc();
+    uint64 mask = 0;
+    acquire(&p->lock);
+    for (int i=0;i<len;i++) {
+        pte_t* pte = walk(p->pagetable, addr + i*PGSIZE, 0);
+        if (*pte & PTE_A) {
+            mask |= (1 << i);
+            *pte ^= PTE_A;
+        }
+    }
+    release(&p->lock);
+    if(copyout(p->pagetable, mask_addr, (char *)&mask, sizeof(mask)) < 0)
+      return -1;
+    return 0;
 }
 #endif
 
-uint64
-sys_kill(void)
-{
-  int pid;
+uint64 sys_kill(void) {
+    int pid;
 
-  argint(0, &pid);
-  return kill(pid);
+    argint(0, &pid);
+    return kill(pid);
 }
 
 // return how many clock tick interrupts have occurred
 // since start.
-uint64
-sys_uptime(void)
-{
-  uint xticks;
+uint64 sys_uptime(void) {
+    uint xticks;
 
-  acquire(&tickslock);
-  xticks = ticks;
-  release(&tickslock);
-  return xticks;
+    acquire(&tickslock);
+    xticks = ticks;
+    release(&tickslock);
+    return xticks;
 }
