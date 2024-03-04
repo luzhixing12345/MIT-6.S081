a8e0a84c3fa0d288ef856a95d7eeabab2181d447
luzhixing12345 <luzhixing12345@163.com>
2024-03-04 23:38:31 +0800

    [dev] : finish better find with grep match

--- user/find.c
+++ user/find.c
@@ -3,6 +3,46 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+
+int matchhere(char*, char*);
+int matchstar(int, char*, char*);
+
+int
+match(char *re, char *text)
+{
+  if(re[0] == '^')
+    return matchhere(re+1, text);
+  do{  // must look at empty string
+    if(matchhere(re, text))
+      return 1;
+  }while(*text++ != '\0');
+  return 0;
+}
+
+// matchhere: search for re at beginning of text
+int matchhere(char *re, char *text)
+{
+  if(re[0] == '\0')
+    return 1;
+  if(re[1] == '*')
+    return matchstar(re[0], re+2, text);
+  if(re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if(*text!='\0' && (re[0]=='.' || re[0]==*text))
+    return matchhere(re+1, text+1);
+  return 0;
+}
+
+// matchstar: search for c*re at beginning of text
+int matchstar(int c, char *re, char *text)
+{
+  do{  // a * matches zero or more instances
+    if(matchhere(re, text))
+      return 1;
+  }while(*text!='\0' && (*text++==c || c=='.'));
+  return 0;
+}
+
 char *fmtname(char *path) {
     static char buf[DIRSIZ + 1];
     char *p;
@@ -60,7 +100,7 @@ void find(char *path, char *name) {
         if (st.type == T_DIR && strcmp(p, ".") != 0 && strcmp(p, "..") != 0) {
             // 递归搜索除 . .. 外的子目录
             find(buf, name);
-        } else if (strcmp(name, p) == 0) {
+        } else if (match(name, p)) {
             printf("%s\n", buf);
         }
     }
